f82f174488f3e8d350965467c73303d72a3efc31
alisadat77 <alisa.dt77@gmail.com>
2021-12-23 19:15:58 +0330

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,91 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 11, y = 10;
+
+        //System.out.println( x *=2);
+        //comparison operators
+//        System.out.println(x == y);
+//        System.out.println(x != y);
+//        System.out.println(x > y);
+//        System.out.println(x < y);
+//        System.out.println(x >= y);
+//        System.out.println(x <= y);
+        //if .... else
+//        if (x > y) {
+//            System.out.println(x +" is greater than "+ y);
+//        } else {
+//            System.out.println(x +" is less than or equal to "+ y);
+//        }
+        // else if
+//        if (x > y) {
+//            System.out.println(x + " is greater than " + y);
+//        } else if (x == y) {
+//            System.out.println(x + " is equal to " + y);
+//        } else {
+//            System.out.println(x + " is less than  " + y);
+//        }
+        //ternery operator
+//        String text = x>y ? ( x + " is greater than " + y ): (x + " is less than  " + y) ;
+//        System.out.println(text);
+
+        //swich
+
+//        switch (dayOfWeek) {
+//            case 1 : //dayOfWeek == 1
+//                System.out.println("Satuarday");
+//                break;
+//            case 2 :
+//                System.out.println("Sunday");
+//                break;
+//            case 3 :
+//                System.out.println("Monday");
+//                break;
+//            case 4 :
+//                System.out.println("Thursday");
+//                break;
+//            case 5 :
+//                System.out.println("Wednesday");
+//                break;
+//            case 6 :
+//                System.out.println("Tuesday");
+//                break;
+//            case 7 :
+//                System.out.println("Friday");
+//            default:
+//                System.out.println("pashmam");
+//        }
+        int dayOfWeek = 5;
+            if (dayOfWeek == 1)
+                System.out.println("Satuarday");
+            else if (dayOfWeek == 2)
+                System.out.println("Sunday");
+            else if (dayOfWeek == 3)
+                System.out.println("Monday");
+            else if (dayOfWeek == 4)
+                System.out.println("Thursday");
+            else if (dayOfWeek == 5)
+                System.out.println("Wednesday");
+            else if (dayOfWeek == 6)
+                System.out.println("Tuesday");
+            else if (dayOfWeek == 7)
+                System.out.println("Friday");
+            else
+                System.out.println(" this number is not in weekdays");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
